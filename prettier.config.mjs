bd4135bf54625784e096caa6a11656eb9dd9fe14
David Bauch <jamesbauch@gmail.com>
2025-07-06 12:48:55 -0400

    adding biome. cleaning up repo for personal use

--- prettier.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import("prettier").Config} */
-const config = {
-    printWidth: 100,
-    tabWidth: 4,
-    plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-};
-
-export default config;
